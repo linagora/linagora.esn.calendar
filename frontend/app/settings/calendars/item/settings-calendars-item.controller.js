483526ec71035d069d79efeacc55d0801196ade4
Christophe Hamerling <chamerling@linagora.com>
2017-07-06 15:03:15 +0200

    #715 Split calendars per type

--- frontend/app/settings/calendars/item/settings-calendars-item.controller.js
+++ frontend/app/settings/calendars/item/settings-calendars-item.controller.js
@@ -4,13 +4,18 @@
   angular.module('esn.calendar')
     .controller('CalSettingsCalendarsItemController', CalSettingsCalendarsItemController);
 
-  function CalSettingsCalendarsItemController() {
+  function CalSettingsCalendarsItemController(session, calUIAuthorizationService) {
     var self = this;
 
+    self.canDeleteCalendar = canDeleteCalendar;
     self.remove = remove;
+
+    function canDeleteCalendar() {
+      return calUIAuthorizationService.canDeleteCalendar(self.calendar, session.user._id);
+    }
 
     function remove() {
       self.onRemove(self.calendar);
-      }
+    }
   }
 })();
