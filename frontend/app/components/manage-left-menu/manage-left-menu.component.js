bc220c37e569ed46e8989c73ed8e848c3edbd26c
Christophe Hamerling <chamerling@linagora.com>
2017-07-10 13:03:43 +0200

    #715 Refactor the sidebar manage menu

---
+++ frontend/app/components/manage-left-menu/manage-left-menu.component.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  angular.module('esn.calendar')
+    .component('calManageLeftMenu', {
+      controllerAs: 'ctrl',
+      templateUrl: '/calendar/app/components/manage-left-menu/manage-left-menu.html'
+    });
+})();
