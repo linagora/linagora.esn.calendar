442b2e3b0bf5eb370fb15e809c2bf49dd9f9ea8a
Christophe Hamerling <chamerling@linagora.com>
2017-07-10 13:15:13 +0200

    #715 Move components to the right place

--- frontend/app/calendars-configuration-mobile/subscribe-calendar-menu-item/subscribe-calendar-menu-item.component.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('esn.calendar')
-  .component('calSubscribeCalendarMenuItem', {
-    templateUrl: '/calendar/app/calendars-configuration-mobile/subscribe-calendar-menu-item/subscribe-calendar-menu-item.html'
-  });
---
+++ frontend/app/settings/calendars/fab/create-calendar-menu-item/create-calendar-menu-item.component.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('esn.calendar')
+  .component('calCreateCalendarMenuItem', {
+    templateUrl: '/calendar/app/settings/calendars/fab/create-calendar-menu-item/create-calendar-menu-item.html'
+  });
