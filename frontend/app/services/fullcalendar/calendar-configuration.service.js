c8d78bad3fe0d4831aa528f76a0561312d6e6c63
Maxime Frere <mfrere@linagora.com>
2017-08-01 12:01:31 +0200

    linagora/lgs/openpaas/openpaas-roadmap#15 calFullUiConfiguration services

---
+++ frontend/app/services/fullcalendar/calendar-configuration.service.js
@@ -0,0 +1,63 @@
+(function() {
+  'use strict';
+
+  angular.module('esn.calendar')
+         .factory('calFullUiConfiguration', calFullUiConfiguration);
+
+  function calFullUiConfiguration(
+    $q,
+    esnUserConfigurationService,
+    calBusinessHoursService,
+    _,
+    CAL_UI_CONFIG,
+    CAL_USER_CONFIGURATION
+  ) {
+    var handler = {
+      workingDays: _showWorkingDays
+    };
+
+    var service = {
+      get: get
+    };
+
+    return service;
+
+    ////////////
+
+    function get() {
+      return esnUserConfigurationService.get(CAL_USER_CONFIGURATION.keys, CAL_USER_CONFIGURATION.moduleName)
+        .then(function(configurations) {
+          var setConfigurations = configurations.map(function(configuration) {
+            if (!configuration.value) {
+              return $q.when({});
+            }
+
+            return handler[configuration.name]();
+          });
+
+          return $q.all(setConfigurations);
+        })
+        .then(function(configurationsSetted) {
+          var uiConfig = angular.copy(CAL_UI_CONFIG);
+
+          configurationsSetted.push(CAL_UI_CONFIG.calendar);
+          uiConfig.calendar = angular.extend.apply(null, configurationsSetted);
+
+          return uiConfig;
+        });
+    }
+
+    function _showWorkingDays() {
+      function hasDowKey(businessHour) {
+        return _.has(businessHour, 'dow');
+      }
+
+      return calBusinessHoursService.getUserBusinessHours().then(function(userBusinessHours) {
+        var workingDays = _.result(_.find(userBusinessHours, hasDowKey), 'dow') || CAL_UI_CONFIG.calendarDefaultDaysValue;
+
+        return { hiddenDays: _.difference(CAL_UI_CONFIG.calendarDaysValue, workingDays) };
+      });
+    }
+  }
+
+})();
---
+++ frontend/app/services/fullcalendar/calendar-configuration.service.spec.js
@@ -0,0 +1,145 @@
+'use strict';
+
+/* global chai, sinon */
+
+var expect = chai.expect;
+
+describe('The calFullUiConfiguration service', function() {
+  var $q,
+  $httpBackend,
+  calFullUiConfiguration,
+  esnConfig,
+  esnUserConfigurationService,
+  moduleName,
+  moduleConfiguration,
+  businessHours,
+  CAL_UI_CONFIG;
+
+  beforeEach(function() {
+    angular.mock.module('esn.calendar');
+    businessHours = [{
+      daysOfWeek: [1, 5, 6],
+      start: '00:00',
+      end: '25:00'
+    }];
+    moduleName = 'linagora.esn.calendar';
+    moduleConfiguration = ['workingDays'];
+    esnConfig = sinon.spy(function() {
+      return $q.when(businessHours);
+    });
+
+    angular.mock.module(function($provide) {
+      $provide.value('esnConfig', esnConfig);
+    });
+  });
+
+  beforeEach(angular.mock.inject(function(
+    _$rootScope_,
+    _$q_,
+    _$httpBackend_,
+    _calFullUiConfiguration_,
+    _esnUserConfigurationService_,
+    _CAL_UI_CONFIG_
+  ) {
+    calFullUiConfiguration = _calFullUiConfiguration_;
+    esnUserConfigurationService = _esnUserConfigurationService_;
+    $q = _$q_;
+    $httpBackend = _$httpBackend_;
+    CAL_UI_CONFIG = _CAL_UI_CONFIG_;
+  }));
+
+  describe('The get function', function() {
+    it('should return the default configuration if no configuration value', function(done) {
+      var payload = [
+        {
+          name: moduleName,
+          keys: moduleConfiguration
+        }
+      ];
+      var httpResponse = [
+        {
+          name: moduleName,
+          configurations: [{
+            name: 'workingDays',
+            value: null
+          }]
+        }
+      ];
+
+      sinon.spy(esnUserConfigurationService, 'get');
+      $httpBackend.expectPOST('/api/configurations?scope=user', payload).respond(httpResponse);
+      calFullUiConfiguration.get()
+        .then(function(uiConfiguration) {
+          expect(uiConfiguration).to.deep.equal(CAL_UI_CONFIG);
+          expect(esnUserConfigurationService.get).to.have.been.calledOnce;
+          expect(esnUserConfigurationService.get).to.have.been.calledWith(moduleConfiguration, moduleName);
+
+          done();
+        });
+
+      $httpBackend.flush();
+    });
+    describe('The _setOnlyWorkingDays function', function() {
+      it('should get default hiddenDays if business hours is not defined', function(done) {
+        var expectedResult = [0, 6];
+        var payload = [
+          {
+            name: moduleName,
+            keys: moduleConfiguration
+          }
+        ];
+        var httpResponse = [
+          {
+            name: moduleName,
+            configurations: [{
+              name: 'workingDays',
+              value: true
+            }]
+          }
+        ];
+
+        businessHours = [];
+        sinon.spy(esnUserConfigurationService, 'get');
+        $httpBackend.expectPOST('/api/configurations?scope=user', payload).respond(httpResponse);
+        calFullUiConfiguration.get()
+          .then(function(uiConfiguration) {
+            expect(uiConfiguration.calendar.hiddenDays).to.deep.equal(expectedResult);
+
+            done();
+          });
+
+        $httpBackend.flush();
+      });
+
+      it('should set hiddenDays from  business hours', function(done) {
+        var expectedResult = [0, 2, 3, 4];
+        var payload = [
+          {
+            name: moduleName,
+            keys: moduleConfiguration
+          }
+        ];
+        var httpResponse = [
+          {
+            name: moduleName,
+            configurations: [{
+              name: 'workingDays',
+              value: []
+            }]
+          }
+        ];
+
+        sinon.spy(esnUserConfigurationService, 'get');
+        $httpBackend.expectPOST('/api/configurations?scope=user', payload).respond(httpResponse);
+        calFullUiConfiguration.get()
+          .then(function(uiConfiguration) {
+            expect(uiConfiguration.calendar.hiddenDays).to.deep.equal(expectedResult);
+
+            done();
+          });
+
+        $httpBackend.flush();
+      });
+    });
+  });
+});
