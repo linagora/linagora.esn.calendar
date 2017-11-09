c1711063cd016b8cb30996b4bff0011bbf528ac1
Christophe Hamerling <chamerling@linagora.com>
2017-11-09 17:44:09 +0100

    #929 Move out community stuff in a dedicated folder

--- frontend/app/routes.js
+++ frontend/app/routes.js
@@ -1,35 +1,11 @@
-(function() {
+(function(angular) {
   'use strict';
 
   angular.module('esn.calendar')
     .config(routesConfig);
 
-  function routesConfig($stateProvider, routeResolver) {
+  function routesConfig($stateProvider) {
     $stateProvider
-      .state('calendarForCommunities', {
-        url: '/calendar/communities/:community_id',
-        templateUrl: '/calendar/app/calendar/community-calendar',
-        abstract: true,
-        resolve: {
-          community: routeResolver.api('communityAPI', 'get', 'community_id', '/communities')
-        },
-        reloadOnSearch: false
-      })
-      .state('calendarForCommunities.main', {
-        url: '',
-        views: {
-          content: {
-            template: '<calendar-view calendar-home-id="calendarHomeId" ui-config="uiConfig"/>',
-            controller: function($scope, community, CAL_UI_CONFIG) {
-              $scope.calendarHomeId = community._id;
-              $scope.uiConfig = angular.copy(CAL_UI_CONFIG);
-              $scope.uiConfig.calendar.editable = false;
-              $scope.uiConfig.calendar.selectable = false;
-            }
-          }
-        }
-      })
-
       .state('calendar', {
         url: '/calendar',
         templateUrl: '/calendar/app/calendar/user-calendar',
@@ -194,4 +170,4 @@
         }
       });
   }
-})();
+})(angular);
