a3b7520ba6d5f05882a88a18c8420c90b16c98fc
Christophe Hamerling <chamerling@linagora.com>
2017-11-13 13:02:37 +0100

    #929 Style the event display

--- frontend/app/services/fullcalendar/planning-render-event.service.js
+++ frontend/app/services/fullcalendar/planning-render-event.service.js
@@ -9,14 +9,25 @@
     calUIAuthorizationService
   ) {
     return function(calendar) {
-      return function(event) {
+      return function(event, element) {
         setEventRights();
+        switchTableElements();
 
         function setEventRights() {
           if (!calUIAuthorizationService.canModifyEvent(calendar, event, session.user._id)) {
             event.startEditable = false;
             event.durationEditable = false;
           }
+        }
+
+        function switchTableElements() {
+          // element is a tr with 3 tds: time, dot, description
+          // here we put the dot first
+          var tr = element[0];
+          var tds = tr.children;
+          var dot = element[0].removeChild(tds[1]);
+
+          tr.insertBefore(dot, tds[0]);
         }
       };
     };
