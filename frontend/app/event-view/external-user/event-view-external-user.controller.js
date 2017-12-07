d437f29f19abed26de2ef917ded93082e127a9a2
Maxime Frere <mfrere@linagora.com>
2017-12-07 16:42:24 +0100

    #1044 event view component - external user change participation

--- frontend/app/event-view/external-user/event-view-external-user.controller.js
+++ frontend/app/event-view/external-user/event-view-external-user.controller.js
@@ -4,13 +4,24 @@
   angular.module('esn.calendar')
     .controller('CalEventViewExternalUserController', CalEventViewExternalUserController);
 
-  function CalEventViewExternalUserController() {
+  function CalEventViewExternalUserController($http) {
     var self = this;
 
     self.$onInit = $onInit;
+    self.changeParticipation = changeParticipation;
 
     function $onInit() {
       self.userAsAttendee = Object.create(self.externalAttendee);
+      self.linksMapping = {
+        ACCEPTED: self.links.yes,
+        TENTATIVE: self.links.maybe,
+        DECLINED: self.links.no
+      };
+    }
+
+    function changeParticipation(partstat) {
+      self.userAsAttendee.partstat = partstat;
+      $http({ method: 'GET', url: self.linksMapping[partstat] });
     }
   }
 })();
