49a8c7803a470e5da885f18f71fa2807fd262fff
Christophe Hamerling <chamerling@linagora.com>
2017-12-11 10:28:47 +0100

    #1101 Remove old templates

--- frontend/app/settings/resources/settings-resources.controller.js
+++ frontend/app/settings/resources/settings-resources.controller.js
@@ -4,20 +4,9 @@
   angular.module('esn.calendar')
     .controller('CalSettingsResourcesController', CalSettingsResourcesController);
 
-  function CalSettingsResourcesController($modal, CAL_RESOURCE) {
+  function CalSettingsResourcesController(CAL_RESOURCE) {
     var self = this;
 
     self.type = CAL_RESOURCE.type;
-    self.openCreateModal = openCreateModal;
-
-    function openCreateModal() {
-      return $modal({
-        templateUrl: '/calendar/app/settings/resources/create/settings-resources-create-modal.html',
-        controller: 'CalSettingsResourcesCreateController',
-        controllerAs: 'ctrl',
-        backdrop: 'static',
-        placement: 'center'
-      });
-    }
   }
 })();
