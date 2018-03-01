613c0829ae2be26da8aa7c0eb72ce50a8d924220
Renaud BOYER <rboyer@linagora.com>
2018-03-01 12:11:41 +0100

    #1147: Fix attendee participation reset when modifying recurring event

--- frontend/app/services/event-service.js
+++ frontend/app/services/event-service.js
@@ -324,15 +324,6 @@
       function modifyEvent(path, event, oldEvent, etag, onCancel, options) {
         oldEvent = oldEventStore[event.uid] = oldEventStore[event.uid] || oldEvent;
         options = options || {};
-        if (event.isInstance()) {
-          return event.getModifiedMaster().then(function(newMasterEvent) {
-            var oldMasterEvent = newMasterEvent.clone();
-
-            oldMasterEvent.modifyOccurrence(oldEvent, true);
-
-            return modifyEvent(path, newMasterEvent, oldMasterEvent, etag, onCancel, options);
-          });
-        }
 
         if (calEventUtils.hasSignificantChange(event, oldEvent)) {
           event.changeParticipation('NEEDS-ACTION');
@@ -356,7 +347,9 @@
           calendarEventEmitter.emitModifiedEvent(oldEvent);
         }
 
-        return calEventAPI.modify(path, event.vcalendar, etag)
+        return event.getModifiedMaster().then(function(masterEvent) {
+          return calEventAPI.modify(path, masterEvent.vcalendar, etag);
+        })
           .then(function(id) {
             event.gracePeriodTaskId = taskId = id;
             calCachedEventSource.registerUpdate(event);
