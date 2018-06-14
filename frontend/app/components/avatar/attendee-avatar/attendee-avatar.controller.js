5b9152d699bde3ccea802e65365ea6523fd33e1e
Christophe Hamerling <chamerling@linagora.com>
2018-06-14 14:43:05 +0200

    #1342 Fix the attendee avatar display

--- frontend/app/components/avatar/attendee-avatar/attendee-avatar.controller.js
+++ frontend/app/components/avatar/attendee-avatar/attendee-avatar.controller.js
@@ -4,19 +4,10 @@
   angular.module('esn.calendar')
     .controller('CalAttendeeAvatarController', CalAttendeeAvatarController);
 
-  function CalAttendeeAvatarController(esnAvatarUrlService) {
+  function CalAttendeeAvatarController() {
     var self = this;
 
-    self.$onChanges = $onChanges;
     self.getDisplayName = getDisplayName;
-
-    function $onChanges() {
-      self.avatarUrl = getUserAvatar();
-    }
-
-    function getUserAvatar() {
-      return esnAvatarUrlService.generateUrl(self.attendee.email);
-    }
 
     function getDisplayName() {
       return self.attendee.name || self.attendee.displayName;
