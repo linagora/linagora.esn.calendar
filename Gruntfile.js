020554f5159d833178172fa64f4631ead0fb0c67
Loic AUBLET <laublet@linagora.com>
2019-04-01 15:39:56 +0200

    esn#2454 add chinese translation

--- Gruntfile.js
+++ Gruntfile.js
@@ -113,7 +113,7 @@ module.exports = function(grunt) {
           }],
           verifyOptions: {
             defaultLocale: 'en',
-            locales: ['en', 'fr', 'vi'],
+            locales: ['en', 'fr', 'vi', 'zh'],
             rules: [
               'all-keys-translated',
               'all-locales-present',
