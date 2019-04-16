c14600d923c653ab3f928d48506e3efd6504f513
Tuan Tuan LE <tlcong@linagora.com>
2019-04-16 11:57:33 +0700

    #1600 clean unit test for search index module

--- test/unit-backend/lib/search/index.js
+++ test/unit-backend/lib/search/index.js
@@ -1,42 +1,38 @@
-'use strict';
-
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const mockery = require('mockery');
 const sinon = require('sinon');
 const _ = require('lodash');
-const ObjectId = require('bson').ObjectId;
+const { ObjectId } = require('bson');
 
 describe('The calendar search Module', function() {
   let pubsubListen, deps, dependencies;
+  let getModule;
 
   beforeEach(function() {
     deps = {
       elasticsearch: {},
       logger: {
-        error: function() {},
-        debug: function() {},
-        info: function() {},
-        warning: function() {}
+        error: () => {},
+        debug: () => {},
+        info: () => {},
+        warning: () => {}
       }
     };
 
-    dependencies = function(name) {
-      return deps[name];
-    };
+    dependencies = name => deps[name];
     pubsubListen = sinon.spy();
-    mockery.registerMock('./pubsub', _.constant({listen: pubsubListen}));
+    mockery.registerMock('./pubsub', _.constant({ listen: pubsubListen }));
+
+    getModule = () => require('../../../../backend/lib/search')(dependencies);
   });
 
   describe('The listen function', function() {
-
     it('should register listeners', function() {
-      var register = sinon.spy();
+      const register = sinon.spy();
 
-      mockery.registerMock('./searchHandler', _.constant({register: register}));
+      mockery.registerMock('./searchHandler', _.constant({ register: register }));
 
-      var module = require('../../../../backend/lib/search')(dependencies);
-
-      module.listen();
+      getModule().listen();
       expect(register).to.have.been.calledOnce;
       expect(pubsubListen).to.have.been.calledOnce;
     });
@@ -45,31 +41,28 @@ describe('The calendar search Module', function() {
   describe('The indexEvent function', function() {
 
     it('should send back error when listener is not started', function(done) {
-      var module = require('../../../../backend/lib/search')(dependencies);
-
-      module.indexEvent({}, this.helpers.callbacks.errorWithMessage(done, 'Event search is not initialized'));
+      getModule().indexEvent({}, this.helpers.callbacks.errorWithMessage(done, 'Event search is not initialized'));
     });
 
     describe('When initialized', function() {
-
       it('should send back error when contact is undefined', function(done) {
-        mockery.registerMock('./searchHandler', _.constant({register: _.constant({})}));
-        var module = require('../../../../backend/lib/search')(dependencies);
+        mockery.registerMock('./searchHandler', _.constant({ register: _.constant({}) }));
+        const module = getModule();
 
         module.listen();
         module.indexEvent(null, this.helpers.callbacks.errorWithMessage(done, 'Event is required'));
       });
 
       it('should call the indexData handler', function(done) {
-        var contact = {id: '123', firstName: 'Bruce'};
-        var indexDataMock = sinon.spy(function(data, callback) {
+        const contact = { id: '123', firstName: 'Bruce' };
+        const indexDataMock = sinon.spy((data, callback) => {
           expect(data).to.deep.equal(contact);
           callback();
         });
 
-        mockery.registerMock('./searchHandler', _.constant({register: _.constant({indexData: indexDataMock})}));
+        mockery.registerMock('./searchHandler', _.constant({ register: _.constant({ indexData: indexDataMock }) }));
 
-        var module = require('../../../../backend/lib/search')(dependencies);
+        const module = getModule();
 
         module.listen();
         module.indexEvent(contact, this.helpers.callbacks.noError(done));
@@ -80,32 +73,29 @@ describe('The calendar search Module', function() {
   describe('The removeEventFromIndex function', function() {
 
     it('should send back error when listener is not started', function(done) {
-      var module = require('../../../../backend/lib/search')(dependencies);
-
-      module.removeEventFromIndex({}, this.helpers.callbacks.errorWithMessage(done, 'Event search is not initialized'));
+      getModule().removeEventFromIndex({}, this.helpers.callbacks.errorWithMessage(done, 'Event search is not initialized'));
     });
 
     describe('When initialized', function() {
 
       it('should send back error when contact is undefined', function(done) {
-
-        mockery.registerMock('./searchHandler', _.constant({register: _.constant({})}));
-        var module = require('../../../../backend/lib/search')(dependencies);
+        mockery.registerMock('./searchHandler', _.constant({ register: _.constant({}) }));
+        const module = getModule();
 
         module.listen();
         module.removeEventFromIndex(null, this.helpers.callbacks.errorWithMessage(done, 'Event is required'));
       });
 
       it('should call the removeFromIndex handler', function(done) {
-        var contact = {id: '123', firstName: 'Bruce'};
+        const contact = {id: '123', firstName: 'Bruce'};
 
-        function removeFromIndexMock(data, callback) {
+        const removeFromIndexMock = (data, callback) => {
           expect(data).to.deep.equal(contact);
           callback();
-        }
+        };
 
-        mockery.registerMock('./searchHandler', _.constant({register: _.constant({removeFromIndex: removeFromIndexMock})}));
-        var module = require('../../../../backend/lib/search')(dependencies);
+        mockery.registerMock('./searchHandler', _.constant({ register: _.constant({ removeFromIndex: removeFromIndexMock }) }));
+        const module = getModule();
 
         module.listen();
         module.removeEventFromIndex(contact, this.helpers.callbacks.noError(done));
@@ -115,7 +105,7 @@ describe('The calendar search Module', function() {
 
   describe('The searchEvents function', function() {
     it('should call search.searchDocuments with right parameters using default parameters for unset ones', function() {
-      var query = {
+      const query = {
         search: 'Bruce',
         offset: 10,
         limit: 100,
@@ -124,13 +114,12 @@ describe('The calendar search Module', function() {
 
       deps.elasticsearch.searchDocuments = sinon.spy();
 
-      var module = require('../../../../backend/lib/search')(dependencies);
-      var searchConstants = require('../../../../backend/lib/constants').SEARCH;
-      var defaultSort = {};
+      const searchConstants = require('../../../../backend/lib/constants').SEARCH;
+      const defaultSort = {};
 
       defaultSort[searchConstants.DEFAULT_SORT_KEY] = { order: searchConstants.DEFAULT_SORT_ORDER };
 
-      module.searchEvents(query);
+      getModule().searchEvents(query);
       expect(deps.elasticsearch.searchDocuments).to.have.been.calledWith(sinon.match({
         index: 'events.idx',
         type: 'events',
@@ -143,7 +132,7 @@ describe('The calendar search Module', function() {
     });
 
     it('should be able to search document with limit = 0', function() {
-      var query = {
+      const query = {
         search: 'Bruce',
         offset: 10,
         limit: 0,
@@ -152,13 +141,12 @@ describe('The calendar search Module', function() {
 
       deps.elasticsearch.searchDocuments = sinon.spy();
 
-      var module = require('../../../../backend/lib/search')(dependencies);
-      var searchConstants = require('../../../../backend/lib/constants').SEARCH;
-      var defaultSort = {};
+      const searchConstants = require('../../../../backend/lib/constants').SEARCH;
+      const defaultSort = {};
 
       defaultSort[searchConstants.DEFAULT_SORT_KEY] = { order: searchConstants.DEFAULT_SORT_ORDER };
 
-      module.searchEvents(query);
+      getModule().searchEvents(query);
       expect(deps.elasticsearch.searchDocuments).to.have.been.calledWith(sinon.match({
         index: 'events.idx',
         type: 'events',
@@ -171,7 +159,7 @@ describe('The calendar search Module', function() {
     });
 
     it('should call search.searchDocuments with right parameters', function() {
-      var query = {
+      const query = {
         search: 'Bruce',
         offset: 10,
         limit: 100,
@@ -182,9 +170,7 @@ describe('The calendar search Module', function() {
 
       deps.elasticsearch.searchDocuments = sinon.spy();
 
-      var module = require('../../../../backend/lib/search')(dependencies);
-
-      module.searchEvents(query);
+      getModule().searchEvents(query);
       expect(deps.elasticsearch.searchDocuments).to.have.been.calledWith(sinon.match({
         index: 'events.idx',
         type: 'events',
@@ -199,7 +185,7 @@ describe('The calendar search Module', function() {
     });
 
     it('should send back error when search.searchDocuments fails', function(done) {
-      var query = {
+      const query = {
         search: 'Bruce',
         offset: 10,
         limit: 100,
@@ -210,33 +196,27 @@ describe('The calendar search Module', function() {
         return callback(new Error());
       };
 
-      var module = require('../../../../backend/lib/search')(dependencies);
-
-      module.searchEvents(query, this.helpers.callbacks.error(done));
+      getModule().searchEvents(query, this.helpers.callbacks.error(done));
     });
 
     it('should send back result when search.searchDocuments is successful', function(done) {
-      var query = {
+      const query = {
         search: 'Bruce',
         offset: 10,
         limit: 100,
         userId: new ObjectId()
       };
-      var total = 10;
-      var hits = [{_id: 1}, {_id: 2}];
+      const total = 10;
+      const hits = [{_id: 1}, {_id: 2}];
 
-      deps.elasticsearch.searchDocuments = function(options, callback) {
-        return callback(null, {
-          hits: {
-            total: total,
-            hits: hits
-          }
-        });
-      };
+      deps.elasticsearch.searchDocuments = (options, callback) => callback(null, {
+        hits: {
+          total: total,
+          hits: hits
+        }
+      });
 
-      var module = require('../../../../backend/lib/search')(dependencies);
-
-      module.searchEvents(query, function(err, result) {
+      getModule().searchEvents(query, function(err, result) {
         expect(err).to.not.exist;
         expect(result.total_count).to.equal(total);
         expect(result.list).to.deep.equal(hits);
@@ -246,11 +226,10 @@ describe('The calendar search Module', function() {
   });
 
   describe('The searchNextEvent function', function() {
-
     it('should call search.searchDocuments with right parameters', function() {
       deps.elasticsearch.searchDocuments = sinon.spy();
 
-      require('../../../../backend/lib/search')(dependencies).searchNextEvent({ id: 'userId' });
+      getModule().searchNextEvent({ id: 'userId' });
 
       expect(deps.elasticsearch.searchDocuments).to.have.been.calledWith({
         index: 'events.idx',
@@ -284,7 +263,5 @@ describe('The calendar search Module', function() {
         }
       });
     });
-
   });
-
 });
