f3bca55653dd81c2274c9bec087f04c2e86e932b
Huy Hoang TRINH <hhtrinh@linagora.com>
2020-05-18 15:45:53 +0700

    linagora.esn.seed#12 Add Webpack

--- backend/webserver/config/views.js
+++ backend/webserver/config/views.js
@@ -2,10 +2,11 @@
 
 const express = require('express');
 const FRONTEND_PATH = require('../constants').FRONTEND_PATH;
+const FRONTEND_PATH_BUILD = require('../constants').FRONTEND_PATH_BUILD;
 const CORE_FRONTEND_PATH = require('../constants').CORE_FRONTEND_PATH;
 
 module.exports = (dependencies, application) => {
-  application.use(express.static(FRONTEND_PATH));
+  application.use(express.static(FRONTEND_PATH_BUILD));
   application.set('views', [`${FRONTEND_PATH}/app`, `${FRONTEND_PATH}/event-consultation-app`]);
   application.get('/app/*', (req, res) => {
     const templateName = req.params[0].replace(/\.html$/, '');
