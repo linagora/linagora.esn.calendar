1c61f3e435a80dc8c2fe3a5187f5e2df8872458e
Bao Ngoc PHAM <bnpham@linagora.com>
2020-06-24 13:54:09 +0700

    #1804 Update date-picker in event-suggestion form

--- frontend/app/components/event-date-suggestion/event-date-suggestion.controller.js
+++ frontend/app/components/event-date-suggestion/event-date-suggestion.controller.js
@@ -4,7 +4,7 @@
   angular.module('esn.calendar')
     .controller('calEventDateSuggestionController', calEventDateSuggestionController);
 
-  function calEventDateSuggestionController(calMoment, esnI18nDateFormatService) {
+  function calEventDateSuggestionController(calMoment, esnI18nDateFormatService, esnDatetimeService) {
     var self = this;
 
     self.$onInit = $onInit;
@@ -18,13 +18,18 @@
       self.dateFormat = esnI18nDateFormatService.getLongDateFormat();
       self.full24HoursDay = self.event.full24HoursDay;
       // on load, ensure that duration between start and end is stored inside editedEvent
+      self.start = calMoment(self.event.start);
+      // In CalDAV backend, the end date of a full-24-hours-day event is stored +1 day compared to the end date when a user saves the event.
+      // Therefore, if this is a full-24-hours-day event, we need to display -1 day for the end date input.
+      self.end = !self.full24HoursDay ? calMoment(self.event.end) : calMoment(self.event.end).subtract(1, 'days');
       self.onEndDateChange();
+      _updateMinEndDate();
     }
 
     function dateOnBlurFn() {
       //this is used to re-update views from the model in case the view is cleared
-      self.event.start = self.event.start.clone();
-      self.event.end = self.event.end.clone();
+      self.start = self.start.clone();
+      self.end = self.end.clone();
     }
 
     function getMinDate() {
@@ -42,39 +47,63 @@
         self.previousStart = self.event.start.clone();
         self.previousEnd = self.event.end.clone();
 
-        start = self.event.start.stripTime();
-        end = self.event.end.stripTime().add(1, 'days');
+        start = self.start.stripTime();
+        end = self.end.stripTime();
       } else if (self.previousStart && self.previousEnd) {
         start = self.previousStart;
         end = self.previousEnd;
       } else {
         var nextHour = calMoment().startOf('hour').add(1, 'hour').hour();
 
-        // We need to set back the utc flag to false here.
-        // See Ambiguously-timed Moments http://fullcalendar.io/docs/utilities/Moment/
-        start = self.event.start.local().startOf('day').hour(nextHour);
-        end = self.event.end.local().startOf('day').subtract(1, 'day').hour(nextHour).add(1, 'hours');
+        start = esnDatetimeService.setAmbigTime(self.start.startOf('day').hour(nextHour), false);
+        end = esnDatetimeService.setAmbigTime(self.end.startOf('day').hour(nextHour).add(1, 'hours'), false);
       }
-      self.event.start = start;
-      self.event.end = end;
-      self.diff = self.event.end.diff(self.event.start);
+      self.start = start;
+      self.end = end;
+      self.diff = self.end.diff(self.start);
+      _syncEventDateTime();
     }
 
     function onStartDateChange() {
-      if (!self.event.start || !self.event.start.isValid()) {
+      if (!self.start || !self.start.isValid()) {
         return;
       }
-      self.event.end = calMoment(self.event.start).add(self.diff / 1000, 'seconds');
+
+      // When 'All day' is selected, strip time
+      if (self.full24HoursDay) {
+        self.start = self.start.stripTime();
+      }
+      self.end = calMoment(self.start).add(self.diff / 1000, 'seconds');
+      _updateMinEndDate();
+      _syncEventDateTime();
     }
 
     function onEndDateChange() {
-      if (!self.event.end || !self.event.end.isValid()) {
+      if (!self.end || !self.end.isValid()) {
         return;
       }
-      if (self.event.end.isBefore(self.event.start)) {
-        self.event.end = calMoment(self.event.start).add(1, 'hours');
+      if (self.end.isBefore(self.start)) {
+        self.end = calMoment(self.start).add(1, 'hours');
       }
-      self.diff = self.event.end.diff(self.event.start);
+      self.diff = self.end.diff(self.start);
+      _updateMinEndDate();
+      _syncEventDateTime();
+    }
+
+    function _updateMinEndDate() {
+      self.minEndDate = getMinDate();
+    }
+
+    function _syncEventDateTime() {
+      if (self.full24HoursDay) {
+        self.event.start = self.start.clone().stripTime();
+        self.event.end = self.end.clone().stripTime().add(1, 'days');
+
+        return;
+      }
+
+      self.event.start = self.start.clone();
+      self.event.end = self.end.clone();
     }
   }
 })();
