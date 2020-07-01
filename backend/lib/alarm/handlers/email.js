b79a2a870597f1fcca12c47a0ad2fe61024a9c9b
Huy Hoang TRINH <hhtrinh@linagora.com>
2020-07-01 14:31:12 +0700

    #1811 - Update Alarm Model State When Job Is Done

--- backend/lib/alarm/handlers/email.js
+++ backend/lib/alarm/handlers/email.js
@@ -6,6 +6,7 @@ module.exports = dependencies => {
   const logger = dependencies('logger');
   const userModule = dependencies('user');
   const jcalHelper = require('../../helpers/jcal');
+  const db = require('../db')(dependencies);
   const i18nHelper = require('../../helpers/i18n')(dependencies);
   const emailModule = require('../../email')(dependencies);
 
@@ -15,7 +16,8 @@ module.exports = dependencies => {
     handle
   };
 
-  function handle({ ics, attendee, eventPath }) {
+  function handle({ _id, ics, attendee, eventPath }) {
+    let error;
     logger.debug(`Sending alarm by email to ${attendee} for event ${eventPath}`);
     const event = jcalHelper.jcal2content(ics);
 
@@ -43,6 +45,29 @@ module.exports = dependencies => {
       }))
       .catch(err => {
         logger.error('Can not send alarm email', err);
+        error = err;
+        throw err;
+      })
+      .finally(() => {
+        if (!error) {
+          return updateAlarmState(_id, CONSTANTS.ALARM.STATE.DONE);
+        }
+
+        return updateAlarmState(_id, CONSTANTS.ALARM.STATE.ERROR);
+      });
+  }
+
+  function updateAlarmState(id, state) {
+    if (!id) {
+      logger.error('calendar:alarm:handlers:email - Failed to update alarm status - No Alarm ID');
+
+      return Promise.resolve();
+    }
+
+    return db.findById(id)
+      .then(alarm => db.setState(alarm, state))
+      .catch(err => {
+        logger.error('calendar:alarm:handlers:email - Failed to update alarm status', err);
         throw err;
       });
   }
