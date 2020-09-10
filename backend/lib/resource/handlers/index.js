54dc4693711797c986a4688aecb7499131c5d6fc
Tuan Tuan LE <tlcong@linagora.com>
2020-09-10 09:18:19 +0700

    https://github.com/OpenPaaS-Suite/esn/issues/33 Sending ACK message is handled in core amqp

--- backend/lib/resource/handlers/index.js
+++ backend/lib/resource/handlers/index.js
@@ -3,7 +3,6 @@ const { EVENTS } = require('../../constants');
 module.exports = dependencies => {
   const logger = dependencies('logger');
   const { pointToPoint: pointToPointMessaging } = dependencies('messaging');
-  const amqpClientProvider = dependencies('amqpClientProvider');
   const requestHandler = require('./request')(dependencies);
   const acceptedHandler = require('./accepted')(dependencies);
   const declinedHandler = require('./declined')(dependencies);
@@ -15,18 +14,8 @@ module.exports = dependencies => {
   function init() {
     logger.debug('Initializing resources handlers');
 
-    pointToPointMessaging.get(EVENTS.RESOURCE_EVENT.CREATED).receive(handleEvent(requestHandler.handle));
-    pointToPointMessaging.get(EVENTS.RESOURCE_EVENT.ACCEPTED).receive(handleEvent(acceptedHandler.handle));
-    pointToPointMessaging.get(EVENTS.RESOURCE_EVENT.DECLINED).receive(handleEvent(declinedHandler.handle));
-
-    function handleEvent(handler) {
-      return (jsonMessage, { ack } = {}) => handler(jsonMessage)
-        .then(amqpClientProvider.getClient)
-        .then(() => typeof ack === 'function' && ack())
-        .catch(err => {
-          logger.error(`CAlResourceRequestHandler[${jsonMessage.uid}] Error`, err);
-          throw err;
-        });
-    }
+    pointToPointMessaging.get(EVENTS.RESOURCE_EVENT.CREATED).receive(requestHandler.handle);
+    pointToPointMessaging.get(EVENTS.RESOURCE_EVENT.ACCEPTED).receive(acceptedHandler.handle);
+    pointToPointMessaging.get(EVENTS.RESOURCE_EVENT.DECLINED).receive(declinedHandler.handle);
   }
 };
--- test/unit-backend/lib/resource/handlers/index.js
+++ test/unit-backend/lib/resource/handlers/index.js
@@ -1,26 +1,13 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const mockery = require('mockery');
-const { EVENTS } = require('../../../../../backend/lib/constants');
 
 describe('The Resource handlers module', function() {
-  let amqpClient, amqpClientProvider, CONSTANTS;
-  let jsonMessage;
-  let pointToPointMock, ackMock, handleFunctions;
+  let CONSTANTS;
+  let pointToPointMock, handleFunctions;
 
   beforeEach(function() {
-    jsonMessage = { baz: 'qix' };
     handleFunctions = {};
-
-    amqpClient = {
-      subscribe: sinon.spy(),
-      ack: sinon.spy()
-    };
-
-    amqpClientProvider = {
-      getClient: sinon.stub().returns(Promise.resolve(amqpClient))
-    };
-
     CONSTANTS = require(`${this.moduleHelpers.modulePath}/backend/lib/constants`);
 
     pointToPointMock = {
@@ -33,9 +20,7 @@ describe('The Resource handlers module', function() {
         handleFunctions[pointToPointMock.currentTopic] = handleFn;
       }
     };
-    ackMock = sinon.spy();
 
-    this.moduleHelpers.addDep('amqpClientProvider', amqpClientProvider);
     this.moduleHelpers.addDep('messaging', {
       pointToPoint: pointToPointMock
     });
@@ -63,44 +48,6 @@ describe('The Resource handlers module', function() {
     it('should subscribe to CONSTANTS.EVENTS.RESOURCE_EVENT.DECLINED', function() {
       this.requireModule().init();
       expect(pointToPointMock.get).to.have.been.calledWith(CONSTANTS.EVENTS.RESOURCE_EVENT.DECLINED);
-    });
-  });
-
-  describe('The handleEvent function', function() {
-    it('should reject when request handler rejects', function(done) {
-      const error = new Error('I failed');
-      const handle = sinon.stub().returns(Promise.reject(error));
-
-      mockery.registerMock('./request', () => ({
-        handle
-      }));
-
-      this.requireModule().init();
-
-      handleFunctions[EVENTS.RESOURCE_EVENT.CREATED](jsonMessage, { ack: ackMock })
-        .then(() => done(new Error('should not resolve')))
-        .catch(err => {
-          expect(handle).to.have.been.calledWith(jsonMessage);
-          expect(err).to.equals(error);
-          done();
-        });
-    });
-
-    it('should ack the message on success', function(done) {
-      const response = 1;
-      const handle = sinon.stub().returns(Promise.resolve(response));
-
-      mockery.registerMock('./request', () => ({
-        handle
-      }));
-
-      this.requireModule().init();
-
-      handleFunctions[EVENTS.RESOURCE_EVENT.CREATED](jsonMessage, { ack: ackMock })
-        .then(() => {
-          expect(ackMock).to.have.been.calledOnce;
-          done();
-        });
     });
   });
 });
